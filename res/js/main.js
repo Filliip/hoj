153004c04e65816c543e2616d814b14704a55169
filipek <filip.rab@student.spsmb.cz>
2023-11-03 09:23:15 +0100

    update

---
+++ res/js/main.js
@@ -0,0 +1,36 @@
+/*
+// params - parametry
+function tomas(item, item2) {
+    console.log("nesu: " + item + "," + item2);
+}
+//args - argumenty
+tomas("mys", "budovu");
+tomas("notebook", "kyblíček")
+
+const ttomas = (item, item2) => {
+    console.log(`nesu:${item}; ${item2}`);
+}
+
+tomas("mys", "budovu");
+tomas("notebook", "kyblíček") 
+
+function play(song, reznik){
+console.log(`playing: ${song} by ${reznik}`)
+}
+
+const playMusic = (song, reznik) => {
+    console.log(`playing: ${song} by ${reznik}`)
+}
+
+play("Zasrany vanoce", "reznik" )
+play("Take me home", "John Denver" ) */
+
+function sum(a, b) {
+  return a + b;
+}
+
+let resultX = sum(9, 9);
+let resultY = sum(6, 15);
+let result = sum(resultX, resultY);
+console.log(result);
+
